252680864c736182e7902caef670c96f17b51b08
samosica <samosic4@gmail.com>
2025-07-27 00:15:37 +0900

    Group logs

--- script/test-all.sh
+++ script/test-all.sh
@@ -4,6 +4,16 @@ set -euo pipefail
 CURDIR=$(cd "$(dirname "$0")" && pwd)
 readonly CURDIR
 
+echo-in-gh-actions(){
+    if [ -n "${GITHUB_ACTION-}" ]; then
+        echo "$@"
+    fi
+}
+
 cd "$CURDIR"
+echo-in-gh-actions '::group::Compilation test'
 ./compilation-test.sh
+echo-in-gh-actions '::endgroup::'
+echo-in-gh-actions '::group::Unit tests'
 ./test.sh
+echo-in-gh-actions '::endgroup::'
