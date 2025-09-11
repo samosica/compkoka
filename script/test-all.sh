82498db1a1e0572afff36c4ae5f7231f2faf7adb
samosica <samosic4@gmail.com>
2025-09-11 19:03:38 +0900

    fix(test): add help message to test-all.sh

--- script/test-all.sh
+++ script/test-all.sh
@@ -4,6 +4,30 @@ set -euo pipefail
 CURDIR=$(cd "$(dirname "$0")" && pwd)
 readonly CURDIR
 
+usage(){
+    cat <<EOF
+Usage: $0
+Perform all tests
+
+Options:
+    -h, --help          help
+
+Environment variables:
+    KOKA_COMPILER           specify compiler path (default: koka)
+EOF
+}
+
+read_args(){
+    while [ $# -ge 1 ]; do
+        case "$1" in
+            -h | --help) usage; exit 0;;
+            *) usage; exit 1;;
+        esac
+    done
+}
+
+read_args "$@"
+
 KOKA_COMPILE_OPTION_SETS=('' '-O2')
 
 cd "$CURDIR"
