c73bab6de3901cbce53bfa96abb731ee409bbd27
samosica <samosic4@gmail.com>
2025-09-12 02:13:16 +0900

    feat(io-test.sh): add test file arguments

--- script/io-test.sh
+++ script/io-test.sh
@@ -6,6 +6,8 @@ readonly SCRIPT_DIR
 
 readonly ROOT_DIR=$SCRIPT_DIR/..
 
+readonly TEST_DIR=$ROOT_DIR/test/io
+
 TEMP_DIR=$(mktemp -d)
 readonly TEMP_DIR
 # shellcheck disable=SC2064
@@ -13,8 +15,11 @@ trap "rm -r $TEMP_DIR" 0
 
 usage(){
     cat <<EOF
-Usage: $0
+Usage: $0 [FILE]...
 Run unit tests using IO
+
+Arguments:
+    FILE                    specify test file to be executed (default: all)
 
 Options:
     -h, --help              help
@@ -26,12 +31,29 @@ EOF
 }
 
 read_args(){
+    FILES=()
+
     while [ $# -ge 1 ]; do
         case "$1" in
             -h | --help) usage; exit 0;;
-            *) usage; exit 1;;
+            -*) usage; exit 1;;
+            *)
+                if ! [ -e "$TEST_DIR/$1" ]; then
+                    error "no such file: $1"; exit 1
+                fi
+                FILES+=("./$1"); shift 1;;
         esac
     done
+
+    if [ "${#FILES[@]}" -eq 0 ]; then
+        pushd "$TEST_DIR" >/dev/null
+        local file
+        while IFS= read -r -d '' file; do
+            FILES+=("$file")
+        done < <(find . -type f -name '*.kk' -print0 | sort -z)
+        popd >/dev/null
+    fi
+    readonly FILES
 
     koka_compiler=${koka_compiler:-koka}
     koka_options=${koka_options-}
@@ -53,7 +75,7 @@ run_test(){
     local out=${prefix}-out
 
     echo -n "Testing $prog: "
-    if ! "$koka_compiler" --no-debug -i"$ROOT_DIR/src" -o "$exe" "$prog" >out 2>&1; then
+    if ! "$koka_compiler" --no-debug -i"$ROOT_DIR/src" -o "$exe" "$TEST_DIR/$prog" >out 2>&1; then
         red "FAILED"
         cat out
         exit 1
@@ -62,7 +84,7 @@ run_test(){
 
     # Note: as of Koka v3.2.2, programs always exit with status code 0 so the following
     #       condition is always true. See <https://github.com/koka-lang/koka/issues/702>.
-    if ! "$exe" <"$in" >out 2>err ; then
+    if ! "$exe" <"$TEST_DIR/$in" >out 2>err ; then
         red "FAILED"
         echo '[output]'
         cat out
@@ -74,7 +96,7 @@ run_test(){
 
     # shellcheck disable=SC2238
     # shellcheck disable=SC2094
-    if ! diff out "$out" >diff 2>&1; then
+    if ! diff out "$TEST_DIR/$out" >diff 2>&1; then
         red "FAILED"
         cat diff
         exit 1
@@ -84,12 +106,11 @@ run_test(){
 }
 
 run_tests(){
-    cp -r "$ROOT_DIR"/test/io/* "$TEMP_DIR"
     cd "$TEMP_DIR"
 
-    while IFS= read -r -d '' prog; do
-        run_test "$prog"
-    done < <(find . -type f -name '*.kk' -print0 | sort -z)
+    for file in "${FILES[@]}"; do
+        run_test "$file"
+    done
 }
 
 read_args "$@"
